69a4c366a63030018683eb11fa61780395882c1b
ashes303 <ash96@pitt.edu>
2018-04-12 15:39:58 -0400

    Added triggers for Team 2's app

--- GroupProject/app/src/main/java/group1/pittapi/Building_Info.java
+++ GroupProject/app/src/main/java/group1/pittapi/Building_Info.java
@@ -1,6 +1,11 @@
 package group1.pittapi;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,6 +22,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.gson.Gson;
+
+import com.pavel.cs1699_team22.MusicServiceBinder;
 
 public class Building_Info extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -86,5 +94,68 @@ public class Building_Info extends AppCompatActivity {
     public void onResume(){
         super.onResume();
     }
+
+    /**
+     * This function displays how to use the trivia trigger.
+     * @param v
+     */
+    public void triviaTrigger(View v)
+    {
+        triviaFactTriggerFromTeam1("Just like how the Empire State Building in NYC changes its lights for special occasions, the upper section of the Cathedral of Learning illuminates gold with “victory lights” after a football team victory.");
+    }
+
+    public void triggerNextApp(View v) {
+        String s = null;
+        TextView textView = findViewById(R.id.buildingInfoTextView);
+        s = textView.getText().toString();
+        triviaFactTriggerFromTeam1(s);
+    }
+
+    String fact = "Sending Nothing"; // The initial fact string
+    MusicServiceBinder mIRemoteService; // A global variable for our custom binder
+    /**
+     * This is our service connection that recieves and uses the custom binder.
+     */
+    ServiceConnection mConnection = new ServiceConnection()
+    {
+        // Called when the connection with the service is established
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // Following the example above for an AIDL interface,
+            // this gets an instance of the IRemoteInterface, which we can use to call on the service
+            // This is the aidl interface object that we can use to call functions of the service
+            mIRemoteService = MusicServiceBinder.Stub.asInterface(service);
+            if(mIRemoteService != null)
+            {
+                try
+                {
+                    mIRemoteService.startMusic(); // Start the music
+                    mIRemoteService.transition(fact); // Send the fact string to the service so it will trigger the next application
+                }
+                catch(Exception e)
+                {
+                    Log.e("debug", e.getMessage());
+                }
+            }
+        }
+        // Called when the connection with the service disconnects unexpectedly
+        public void onServiceDisconnected(ComponentName className) {
+            mIRemoteService = null;
+        }
+    };
+    /**
+     * This function triggers the pantry app with a string that is a trivia fact. Just call it and pass a trivia fact to it.
+     * @param triviaFact A trivia fact
+     */
+    public void triviaFactTriggerFromTeam1(String triviaFact)
+    {
+        Intent intent = new Intent();
+        // This is the information about the service in the other process
+        intent.setComponent(new ComponentName("com.pavel.cs1699_team22", "com.pavel.cs1699_team22.MusicService"));
+        fact = triviaFact; // Grab the trivia fact that was passed in.
+        Context context = getApplicationContext();
+        // Bind the service to this activity
+        bindService(intent,mConnection,Context.BIND_AUTO_CREATE);
+    }
+
 
 }
